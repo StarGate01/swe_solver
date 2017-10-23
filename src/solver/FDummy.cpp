560f93370fa3e70f538203d7511e4c0c7cbcfaff
Christoph Honal <christoph.honal@web.de>
2017-10-23 13:23:55 +0200

    add cxx support

---
+++ src/solver/FDummy.cpp
@@ -0,0 +1,6 @@
+#include "FDummy.hpp"
+
+int FDummy::foo(int bar, int bar2)
+{
+    return (bar + bar2) * bar;
+}
---
+++ src/solver/FDummy.hpp
@@ -0,0 +1,13 @@
+#ifndef FDUMMY_HPP_
+#define FDUMMY_HPP_
+
+class FDummy
+{
+
+public:
+
+    static int foo(int bar, int bar2);
+
+};
+
+#endif /* FDUMMY_HPP_ */
