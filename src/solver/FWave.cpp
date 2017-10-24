299620082b81ccb4585fecc4431321cd8346929f
Christoph Honal <christoph.honal@web.de>
2017-10-24 19:08:49 +0200

    made FWave generic

--- src/solver/FWave.cpp
+++ src/solver/FWave.cpp
@@ -2,3 +2,18 @@
  * @file FWave.cpp
  * @brief Implementation of FWave.hpp
  */
+
+#include "FWave.hpp"
+
+ template <typename T> FWave<T>::computeNetUpdates (T l_h[], T r_h[],
+    T l_hu[], T r_hu[],
+    T b_l, T b_r,	
+    T hNetUpdatesLeft[], T hNetUpdatesRight[],
+    T huNetUpdatesLeft[], T huNetUpdatesRight[],
+    T maxEdgeSpeed )
+{
+        
+}
+
+template class FWave<float>;
+template class FWave<double>;
--- src/solver/FWave.hpp
+++ src/solver/FWave.hpp
@@ -12,33 +12,40 @@
  * @brief Adapter to the surrounding framework
  * 
  * This class provides the necessary methods for the F-Wave environment to interact with the F-Wave solver.
+ * Supports flout and double as type parametrization argument.
+ * 
  */
+template <typename T>
 class FWave
 {
 
 public:
+
     /**
-     * Compute net updates for the left and right cell of the edge. Nothing is returned as the values are changed inside the given matrices.
-     *
+     * Compute net updates for the left and right cell of the edge.
+     * 
+     * Nothing is returned as the values are changed inside the given matrices.
+     * 
      * @param l_h[] The left heights
      * @param r_h[] The right heights
      * @param l_hu[] The left speeds
      * @param r_hu[] The right speeds
      * @param b_l[] The left Bathymetry
      * @param b_r[] The right Bathymetry
-     * @param hNetUpdatesLeft[] The left updates to the heigts
-     * @param hNetUpdatesRight[] The right updates to the heights
-     * @param huNetUpdatesLeft[] The left updates to the speeds
-     * @param huNetUpdatesRight[] The right updates to the speeds
+     * @param[out] hNetUpdatesLeft[] The left updates to the heights
+     * @param[out] hNetUpdatesRight[] The right updates to the heights
+     * @param[out] huNetUpdatesLeft[] The left updates to the speeds
+     * @param[out] huNetUpdatesRight[] The right updates to the speeds
      * @param maxEdgeSpeed maximum wave speed value
      * 
      */
-    void computeNetUpdates (float l_h[], float r_h[],
-        float l_hu[], float r_hu[],
-        float b_l, float b_r,	
-        float hNetUpdatesLeft[], float hNetUpdatesRight[],
-        float huNetUpdatesLeft[], float huNetUpdatesRight[],
-        float maxEdgeSpeed );
+    void computeNetUpdates (T l_h[], T r_h[],
+        T l_hu[], T r_hu[],
+        T b_l, T b_r,	
+        T hNetUpdatesLeft[], T hNetUpdatesRight[],
+        T huNetUpdatesLeft[], T huNetUpdatesRight[],
+        T maxEdgeSpeed );
+
     
 };
 
