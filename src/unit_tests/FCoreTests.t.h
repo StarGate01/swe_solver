b1d9024860d416414541ca12c26032c8fe52d74c
ga96fox <felix.wieser@tum.de>
2017-10-24 12:59:57 +0200

    Improved test documentation

--- src/unit_tests/FCoreTests.t.h
+++ src/unit_tests/FCoreTests.t.h
@@ -22,7 +22,7 @@ class FCoreTestSuite : public CxxTest::TestSuite
 public:
 
     /**
-     * Test to verify that a zero vector is returned by the compute method when fed with parameters ql = qr = {0,0}
+     * @brief Test to verify that a zero vector is returned by the compute method when fed with parameters ql = qr = {0,0}
      */
     void testcompute_zero(void)
     {
@@ -32,17 +32,18 @@ public:
     }
 
     /**
-     * Test the implmentation of FCore::compute against some precomputed values
+     * @brief Test the implmentation of FCore::compute against some precomputed values
      * 
     */
     void testcompute_valid(void)
     {
-        /** Scenario 1 */
+        /** @brief Scenario 1: Zero input results in zero output */
         struct qvector ql = {0.0, 0.0};
         struct qvector qr = {0.0, 0.0};
         struct fresult res = FCore::compute(ql, qr);
         TS_ASSERT(true);
 
+        //TODO: Add correct numbers to verify output
         /** Scenario 2 */
         ql = {0.0, 0.0};
         qr = {0.0, 0.0};
@@ -64,91 +65,94 @@ public:
 
 
     /**
-     * Test to verify FCalc::h_func by testing a set of predetermined inputs
-     * to the #ZERO_PRECISION accuracy defined in FConst
+     * @brief Test to verify FCalc::h_func by testing a set of predetermined inputs
+     *  to the #ZERO_PRECISION accuracy defined in FConst
     */
     void testh_func(void)
     {
-        //(3.0 + 1.0)/2 = 2
+        //@brief Assert %\frac{3.0 + 1.0}{2} = 2% 
         struct qvector ql = {1.0, 2.0};
         struct qvector qr = {3.0, 4.0};
         TS_ASSERT(std::abs(FCalc::h_func(ql, qr) - 2) < ZERO_PRECISION);
 
-        //(-3.0+5.0)/2 = 1
+        //@brief Assert %\frac{-3.0+5.0}{2} = 1%
         ql = {-3.0, 2.0};
         qr = {5.0, 4.0};
         TS_ASSERT(std::abs(FCalc::h_func(ql, qr) - 1) < ZERO_PRECISION);
 
-        //(0.0 + 0.0)/2 = 0
+        //@brief Assert %\frac{0.0 + 0.0}{2} = 0%
         ql = {0.0, 0.0};
         qr = {0.0, 4.0};
         TS_ASSERT(std::abs(FCalc::h_func(ql, qr)) < ZERO_PRECISION); //Test zero input for zero output
 
-        //(NaN + 5.0)/2 = NaN
+        //@brief %\frac{\text{NaN} + 5.0}{2} = \text{NaN}%
         ql = {std::numeric_limits<double>::quiet_NaN(), 0.0};
         qr = {5.0, 4.0};
         TS_ASSERT(std::isnan(FCalc::h_func(ql, qr)));
     }
 
     /**
-     * Test to verify FCalc::u_func by testing a set of predetermined inputs
-     * to the #ZERO_PRECISION accuracy defined in FConst
+     * @brief Test to verify FCalc::u_func by testing a set of predetermined inputs
+     *  to the #ZERO_PRECISION accuracy defined in FConst
     */
     void testu_func(void)
     {
-        /** Scenario 1 */
+        /** @brief Scenario 1: Standard set of numbers */
         struct qvector ql = {1.0, 2.0};
         struct qvector qr = {3.0, 4.0};
         TS_ASSERT(std::abs(FCalc::u_func(ql, qr) - 1.5773502691896257645091487805019) < ZERO_PRECISION);
 
-        /** Scenario 2 */
+        /** @brief Scenario 2: Negative hu values */
         ql = {3.0, -42.0};
         qr = {98.0, -599.0};
         TS_ASSERT(std::abs(FCalc::u_func(ql, qr) - -7.286808700128869) < ZERO_PRECISION);
 
-        /** Scenario 3 */
+        /** @brief Scenario 3: ql.h is NaN*/
         ql = {std::numeric_limits<double>::quiet_NaN(), -42.0};
         qr = {98.0, -599.0};
         TS_ASSERT(std::isnan(FCalc::u_func(ql, qr)));
 
-        /** Scenario 4 */
+        /** @brief Scenario 4: ql.hu is NaN */
         ql = {42.0, std::numeric_limits<double>::quiet_NaN()};
         qr = {98.0, -599.0};
         TS_ASSERT(std::isnan(FCalc::u_func(ql, qr)));
 
-        /** Scenario 5 */
+        /** @brief Scenario 5: qr.h is NaN */
         ql = {98.0, -599.0};
         qr = {std::numeric_limits<double>::quiet_NaN(), -42.0};
         TS_ASSERT(std::isnan(FCalc::u_func(ql, qr)));
 
-        /** Scenario 6 */
+        /** @brief Scenario 6: qr.hu is NaN */
         ql = {98.0, -599.0};
         qr = {42.0, std::numeric_limits<double>::quiet_NaN()};
         TS_ASSERT(std::isnan(FCalc::u_func(ql, qr)));
     }
 
     /**
-     * Test to verify FCalc::f_func by testing a set of predetermined inputs
-     * to the #ZERO_PRECISION accuracy defined in FConst
+     * @brief Test to verify FCalc::f_func by testing a set of predetermined inputs
+     *  to the #ZERO_PRECISION accuracy defined in FConst
     */
     void testf_func(void)
     {
-        /** Scenario 1*/
+        /** @brief Scenario 1: Standard set of numbers */
         struct qvector input = {1.0 ,1.0};
         struct vector2 out = FCalc::f_func(input);
         TS_ASSERT_EQUALS(out.x, 1.0);
         TS_ASSERT(std::abs(out.y - 5.905) < ZERO_PRECISION);
 
+        /**@brief Scenario 2: Negative h value */
         input = {-3.0, 5.0};
         out = FCalc::f_func(input);
         TS_ASSERT_EQUALS(out.x, 5.0);
         TS_ASSERT(std::abs(out.y - 69.145) < ZERO_PRECISION);
 
+        /**@brief Scenario 3: Negative h and hu value*/
         input = {-3.0, -5.0};
         out = FCalc::f_func(input);
         TS_ASSERT_EQUALS(out.x, -5.0);
         TS_ASSERT(std::abs(out.y - 69.145) < ZERO_PRECISION);
 
+        /** @brief Scenario 4: hu is NaN */
         input = {1.0, std::numeric_limits<double>::quiet_NaN()};
         out = FCalc::f_func(input);
         TS_ASSERT(std::isnan(out.x));
